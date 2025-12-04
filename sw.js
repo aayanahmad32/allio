b2ca477c06a0e33adb1fe4223b0730d06042658e
aayanahmad32 <aayan.ahmad004@gmail.com>
2025-12-04 16:37:16 +0530

    v1.0.21

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 const CACHE_NAME = 'allio-pro-cache-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
   '/style.css',
   '/script.js',
   '/manifest.json',
@@ -9,52 +8,34 @@ const urlsToCache = [
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
+// Install event
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
     .then(cache => {
-      console.log('Opened cache');
       return cache.addAll(urlsToCache);
     })
   );
 });
 
+// Fetch event
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
     .then(response => {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      
-      return fetch(event.request).then(response => {
-        // Check if valid response
-        if (!response || response.status !== 200 || response.type !== 'basic') {
-          return response;
-        }
-        
-        // Clone response since it can only be consumed once
-        const responseToCache = response.clone();
-        
-        caches.open(CACHE_NAME)
-          .then(cache => {
-            cache.put(event.request, responseToCache);
-          });
-        
-        return response;
-      });
+      // Return cached version or fetch from network
+      return response || fetch(event.request);
     })
   );
 });
 
+// Activate event
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+          if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
